77cb6bbbafc72864057b6c51a18f2e24f44cc0a4
Lucas Bittencourt <lucas_linhares2011@hotmail.com>
2019-02-26 14:15:45 -0300

    Retornar post de um usuario

--- src/main/java/com/lucasBittencourt/springMongo/resources/UserResource.java
+++ src/main/java/com/lucasBittencourt/springMongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasBittencourt.springMongo.domain.Post;
 import com.lucasBittencourt.springMongo.domain.User;
 import com.lucasBittencourt.springMongo.dto.UserDTO;
 import com.lucasBittencourt.springMongo.services.UserService;
@@ -60,5 +61,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 	
 }
