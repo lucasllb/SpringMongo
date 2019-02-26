2171298d26e3c33fb0200b19f4c9e26b772a1b61
Lucas Bittencourt <lucas_linhares2011@hotmail.com>
2019-02-26 15:24:31 -0300

    Consulta simples com @query

--- src/main/java/com/lucasBittencourt/springMongo/repository/PostRepository.java
+++ src/main/java/com/lucasBittencourt/springMongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucasBittencourt.springMongo.domain.Post;
@@ -13,5 +14,8 @@ import com.lucasBittencourt.springMongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options:'i' }}")
+	List<Post> searchTitle(String text);
 	
 }
