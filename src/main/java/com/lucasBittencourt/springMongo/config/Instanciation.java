96d9e843f7c9b648a052285639f1154b9deb475b
Lucas Bittencourt <lucas_linhares2011@hotmail.com>
2019-02-26 14:11:18 -0300

    Referenciar Post Usuarios

--- src/main/java/com/lucasBittencourt/springMongo/config/Instanciation.java
+++ src/main/java/com/lucasBittencourt/springMongo/config/Instanciation.java
@@ -42,6 +42,9 @@ public class Instanciation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("22/01/2019"), "Partiu 2 ", "Viajarrrrrrrrrrr 2", new AuthoDTO(maria));
 		 
 		postRepository.saveAll(Arrays.asList(post1, post2)); 
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
